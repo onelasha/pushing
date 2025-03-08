fcea74df6e75bd4dcb81c5703fe2c0f52c797175
onelasha <one.lasha@gmail.com>
2025-03-08 10:08:47 -0800

    Initial Commit

---
+++ pushing/pushingApp.swift
@@ -0,0 +1,17 @@
+//
+//  pushingApp.swift
+//  pushing
+//
+//  Created by Lavrenti DeLavrenti on 3/8/25.
+//
+
+import SwiftUI
+
+@main
+struct pushingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ pushingTests/pushingTests.swift
@@ -0,0 +1,17 @@
+//
+//  pushingTests.swift
+//  pushingTests
+//
+//  Created by Lavrenti DeLavrenti on 3/8/25.
+//
+
+import Testing
+@testable import pushing
+
+struct pushingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
